627e9c4586f923ee96abb5f1c0ef1271d7122307
IgnatMaldive <maldive@protonmail.com>
2022-08-24 16:59:04 +0200

    upload

---
+++ scripts/kit.js
@@ -0,0 +1,93 @@
+document.write("bananas")
+document.write("<br>");
+
+const ownerKey = localStorage.getItem('ownerKey');
+const purseKey = localStorage.getItem('purseKey');
+const seed = localStorage.getItem('seed');
+const rate = localStorage.getItem('rate');
+const usdBalance = localStorage.getItem('usdBalance');
+const purseSats = localStorage.getItem('purseSats');
+const hasBackedUp = localStorage.getItem('hasBackedUp');
+const contracts = localStorage.getItem('contracts');
+const sending = localStorage.getItem('sending');
+// const minfo = localStorage.getItem('minfo');
+
+
+//   purseAddr 
+
+
+document.write(ownerKey)
+document.write("<br>")
+document.write(purseKey)
+document.write("<br>")
+document.write(seed)
+document.write("<br>")
+document.write(rate)
+document.write("<br>")
+document.write(usdBalance)
+document.write("<br>")
+document.write(purseSats)
+document.write("<br>")
+document.write(hasBackedUp)
+document.write("<br>")
+document.write(contracts)
+document.write("<br>")
+document.write(sending)
+document.write("<br>")
+// document.write(minfo)
+
+
+// var saldo =  document.getElementById("saldo");
+// var saldosatoshi = purseSats*100000000;
+// saldo.innerHTML=saldosatoshi
+
+
+// var depositAddress =  document.getElementById("depositAddress");
+// depositAddress.innerHTML=minfo
+
+
+// var bookNow =  document.getElementById("bookNow");
+
+// bookNow.style= "display:none"
+
+// var receivecard = document. getElementById("receivecard");
+// receivecard.parentNode.removeChild(receivecard);
+
+// var sendcard = document. getElementById("sendcard");
+// sendcard.parentNode.removeChild(sendcard);
+
+//removeElement("sendcard")
+// removeElement("receivecard")
+// removeElement("homecard")
+
+
+
+// bookNow button functionality
+// var bookNow=document.getElementById("bookNow");
+// bookNow.addEventListener("click", function(){
+//     removeElement("homecard")
+// })
+
+// // receive button functionality
+// var receive=document.getElementById("receive");
+// receive.addEventListener("click", function(){
+//     removeElement("homecard")
+// })
+
+
+// //remove element function
+// function removeElement(elementId) {
+//     // Removes an element from the document.
+//     var element = document.getElementById(elementId);
+//     element.parentNode.removeChild(element);
+// }
+
+
+// // send button functionality
+// var send=document.getElementById("send");
+// send.addEventListener("click", function(){
+
+
+// })
+
+
